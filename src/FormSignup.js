59c36a78b18c30ba115e773197ee6dea1ff601c1
Tomiwa Folorunso <78617644+tommichaels@users.noreply.github.com>
2022-11-22 16:22:31 +0100

    Removed useform, validateinfo and others

--- src/FormSignup.js
+++ src/FormSignup.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import validate from './validateInfo';
-import useForm from './useForm';
 import './Form.css';
 
-const FormSignup = ({ submitForm }) => {
-  const { handleChange, handleSubmit, values, errors } = useForm(
-    submitForm,
-    validate
-  );
+const FormSignup = () => {
+
 
   return (
     <div className='form-content-right'>
-      <form onSubmit={handleSubmit} className='form' noValidate>
+      <form className='form' noValidate>
         <h1>
-          Get started with us today! Create your account by filling out the
-          information below.
+          Numeric Counter
         </h1>
         <div className='form-inputs'>
           <label className='form-label'>Username</label>
@@ -23,53 +17,21 @@ const FormSignup = ({ submitForm }) => {
             type='text'
             name='username'
             placeholder='Enter your username'
-            value={values.username}
-            onChange={handleChange}
           />
-          {errors.username && <p>{errors.username}</p>}
-        </div>
-        <div className='form-inputs'>
-          <label className='form-label'>Email</label>
-          <input
-            className='form-input'
-            type='email'
-            name='email'
-            placeholder='Enter your email'
-            value={values.email}
-            onChange={handleChange}
-          />
-          {errors.email && <p>{errors.email}</p>}
-        </div>
-        <div className='form-inputs'>
-          <label className='form-label'>Password</label>
-          <input
-            className='form-input'
-            type='password'
-            name='password'
-            placeholder='Enter your password'
-            value={values.password}
-            onChange={handleChange}
-          />
-          {errors.password && <p>{errors.password}</p>}
-        </div>
-        <div className='form-inputs'>
-          <label className='form-label'>Confirm Password</label>
-          <input
-            className='form-input'
-            type='password'
-            name='password2'
-            placeholder='Confirm your password'
-            value={values.password2}
-            onChange={handleChange}
-          />
-          {errors.password2 && <p>{errors.password2}</p>}
         </div>
         <button className='form-input-btn' type='submit'>
-          Sign up
+          Increment
         </button>
-        <span className='form-input-login'>
-          Already have an account? Login <a href='/'>here</a>
-        </span>
+        <button className='form-input-btn' type='submit'>
+          Decrement
+        </button>
+        <br></br>
+        <button className='form-input-btn' type='submit'>
+          Count
+        </button>
+        <button className='form-input-btn' type='submit'>
+          getCount
+        </button>
       </form>
     </div>
   );
